openstack-archive/stx-distcloud

Keystone DB sync - update dcorch to use dcdbsync
This commit updates dcorch to use the newly introduced dbsync service
APIs to synchronize identity resources from central cloud to subclouds.
The following identity resources are synced:

- users (local users only)
- user passwords
- projects
- roles
- project role assignments
- token revocation events

Story: 2002842
Task: 22787

Depends-On: https://review.openstack.org/#/c/641498

Change-Id: I0840f42f6c7f380d74950fea165688a4b19a2695
Signed-off-by: Andy Ning <andy.ning@windriver.com>

dcorch/engine/fernet_key_manager.py
@@ -27,7 +27,6 @@ from dcorch.common.i18n import _
 from dcorch.common import manager
 from dcorch.common import utils
 from dcorch.drivers.openstack import sdk_platform as sdk
-from dcorch.objects import subcloud as subcloud_obj
 
 
 FERNET_REPO_MASTER_ID = "keys"
@@ -117,9 +116,26 @@ class FernetKeyManager(manager.Manager):
         self._schedule_work(consts.OPERATION_TYPE_PUT)
 
     def distribute_keys(self, ctxt, subcloud_name):
-        subclouds = subcloud_obj.SubcloudList.get_all(ctxt)
-        for sc in subclouds:
-            if sc.region_name == subcloud_name:
-                subcloud = sc
-                self._schedule_work(consts.OPERATION_TYPE_CREATE, subcloud)
-                break
+        keys = self._get_master_keys()
+        if not keys:
+            LOG.info(_("No fernet keys returned from %s") % consts.CLOUD_0)
+            return
+        resource_info = FernetKeyManager.to_resource_info(keys)
+        key_list = FernetKeyManager.from_resource_info(resource_info)
+        self.update_fernet_repo(subcloud_name, key_list)
+
+    def reset_keys(self, subcloud_name):
+        self.update_fernet_repo(subcloud_name)
+
+    @staticmethod
+    def update_fernet_repo(subcloud_name, key_list=None):
+        try:
+            os_client = sdk.OpenStackDriver(subcloud_name)
+            os_client.sysinv_client.create_fernet_repo(key_list)
+        except (exceptions.ConnectionRefused, exceptions.NotAuthorized,
+                exceptions.TimeOut):
+            LOG.info(_("Update the fernet repo on %s timeout") %
+                     subcloud_name)
+        except Exception as e:
+            error_msg = "subcloud: {}, {}".format(subcloud_name, e.message)
+            LOG.info(_("Fail to update fernet repo %s") % error_msg)

starlingx/distcloud

(cherry picked from commit e9096c7a23abeebc1119b5ba3ed1e7b3a357a2ed)
Depends-On: https://review.opendev.org/#/c/655921
Depends-On: https://review.opendev.org/#/c/655773
Depends-On: https://review.opendev.org/#/c/655776
Depends-On: https://review.opendev.org/#/c/655927
Change-Id: I77c2cc712a1c3dc8a228883c3fea1423e5207dea

dcorch/engine/fernet_key_manager.py
@@ -27,7 +27,6 @@ from dcorch.common.i18n import _
 from dcorch.common import manager
 from dcorch.common import utils
 from dcorch.drivers.openstack import sdk_platform as sdk
-from dcorch.objects import subcloud as subcloud_obj
 
 
 FERNET_REPO_MASTER_ID = "keys"
@@ -117,9 +116,26 @@ class FernetKeyManager(manager.Manager):
         self._schedule_work(consts.OPERATION_TYPE_PUT)
 
     def distribute_keys(self, ctxt, subcloud_name):
-        subclouds = subcloud_obj.SubcloudList.get_all(ctxt)
-        for sc in subclouds:
-            if sc.region_name == subcloud_name:
-                subcloud = sc
-                self._schedule_work(consts.OPERATION_TYPE_CREATE, subcloud)
-                break
+        keys = self._get_master_keys()
+        if not keys:
+            LOG.info(_("No fernet keys returned from %s") % consts.CLOUD_0)
+            return
+        resource_info = FernetKeyManager.to_resource_info(keys)
+        key_list = FernetKeyManager.from_resource_info(resource_info)
+        self.update_fernet_repo(subcloud_name, key_list)
+
+    def reset_keys(self, subcloud_name):
+        self.update_fernet_repo(subcloud_name)
+
+    @staticmethod
+    def update_fernet_repo(subcloud_name, key_list=None):
+        try:
+            os_client = sdk.OpenStackDriver(subcloud_name)
+            os_client.sysinv_client.post_fernet_repo(key_list)
+        except (exceptions.ConnectionRefused, exceptions.NotAuthorized,
+                exceptions.TimeOut):
+            LOG.info(_("Update the fernet repo on %s timeout") %
+                     subcloud_name)
+        except Exception as e:
+            error_msg = "subcloud: {}, {}".format(subcloud_name, e.message)
+            LOG.info(_("Fail to update fernet repo %s") % error_msg)