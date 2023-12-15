b754865fadf30beff56605b80ddc57b3d2b8ced5
rlima <Raphael.Lima@windriver.com>
2023-12-15 11:26:31 -0300

    Improve unit test coverage for dcmanager's orchestrator/states/firmware
    Improves unit test coverage for dcmanager's orchestrator/states/firmware
    functionality from 70% to 100%.
    
    Test plan:
      All of the tests were created taking into account the
    output of 'tox -c tox.ini -e cover' command
    
    Story: 2007082
    Task: 49312
    
    Change-Id: I00895fb7616d0c3eeb54f623a8a1ccd223169bd9
    Signed-off-by: rlima <Raphael.Lima@windriver.com>

--- distributedcloud/dcmanager/tests/unit/orchestrator/states/firmware/test_applying_vim_strategy.py
+++ distributedcloud/dcmanager/tests/unit/orchestrator/states/firmware/test_applying_vim_strategy.py
@@ -1,35 +1,30 @@
 #
-# Copyright (c) 2020, 2022 Wind River Systems, Inc.
+# Copyright (c) 2020, 2022, 2024 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+
 import itertools
 import mock
 
 from dccommon.drivers.openstack import vim
 from dcmanager.common import consts
+from dcmanager.orchestrator.states.base import BaseState
 from dcmanager.orchestrator.states.firmware import applying_vim_strategy
-
-from dcmanager.tests.unit.fakes import FakeVimStrategy
 from dcmanager.tests.unit.orchestrator.states.firmware.test_base \
     import TestFwUpdateState
-
-STRATEGY_READY_TO_APPLY = FakeVimStrategy(state=vim.STATE_READY_TO_APPLY)
-STRATEGY_APPLYING = FakeVimStrategy(state=vim.STATE_APPLYING)
-STRATEGY_APPLIED = FakeVimStrategy(state=vim.STATE_APPLIED)
-STRATEGY_APPLY_FAILED = FakeVimStrategy(state=vim.STATE_APPLY_FAILED)
 
 
 @mock.patch("dcmanager.orchestrator.states.firmware.applying_vim_strategy."
             "DEFAULT_MAX_FAILED_QUERIES", 3)
 @mock.patch("dcmanager.orchestrator.states.firmware.applying_vim_strategy."
-            "DEFAULT_MAX_WAIT_ATTEMPTS", 3)
+            "DEFAULT_MAX_WAIT_ATTEMPTS", 5)
 @mock.patch("dcmanager.orchestrator.states.firmware.applying_vim_strategy."
             "WAIT_INTERVAL", 1)
 class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
 
     def setUp(self):
-        super(TestFwUpdateApplyingVIMStrategyStage, self).setUp()
+        super().setUp()
 
         # set the next state in the chain (when this state is successful)
         self.on_success_state = \
@@ -46,24 +41,20 @@ class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
         self.vim_client.get_strategy = mock.MagicMock()
         self.vim_client.apply_strategy = mock.MagicMock()
 
-        p = mock.patch.object(applying_vim_strategy, 'db_api')
-        self.mock_state_db_api = p.start()
-        self.addCleanup(p.stop)
-        self.mock_state_db_api.strategy_step_update = mock.MagicMock()
-
     def test_applying_vim_strategy_success(self):
         """Test applying a VIM strategy that succeeds"""
 
         # first api query is before the apply
         # remaining api query results are after the apply is invoked
         self.vim_client.get_strategy.side_effect = [
-            STRATEGY_READY_TO_APPLY,
-            STRATEGY_APPLYING,
-            STRATEGY_APPLIED,
+            self._create_fake_strategy(vim.STATE_READY_TO_APPLY),
+            self._create_fake_strategy(vim.STATE_APPLYING),
+            self._create_fake_strategy(vim.STATE_APPLIED),
         ]
 
         # API calls acts as expected
-        self.vim_client.apply_strategy.return_value = STRATEGY_APPLYING
+        self.vim_client.apply_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_APPLYING)
 
         # invoke the strategy state operation on the orch thread
         self.worker.perform_state_action(self.strategy_step)
@@ -76,7 +67,8 @@ class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
         """Test applying a VIM strategy that raises an exception"""
 
         # first api query is before the apply
-        self.vim_client.get_strategy.return_value = STRATEGY_READY_TO_APPLY
+        self.vim_client.get_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_READY_TO_APPLY)
 
         # raise an exception during apply_strategy
         self.vim_client.apply_strategy.side_effect =\
@@ -93,10 +85,12 @@ class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
         """Test applying a VIM strategy that returns a failed result"""
 
         # first api query is before the apply
-        self.vim_client.get_strategy.return_value = STRATEGY_READY_TO_APPLY
+        self.vim_client.get_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_READY_TO_APPLY)
 
         # return a failed strategy
-        self.vim_client.apply_strategy.return_value = STRATEGY_APPLY_FAILED
+        self.vim_client.apply_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_APPLY_FAILED)
 
         # invoke the strategy state operation on the orch thread
         self.worker.perform_state_action(self.strategy_step)
@@ -110,13 +104,14 @@ class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
 
         # first api query is before the apply
         self.vim_client.get_strategy.side_effect = [
-            STRATEGY_READY_TO_APPLY,
-            STRATEGY_APPLYING,
-            STRATEGY_APPLY_FAILED,
+            self._create_fake_strategy(vim.STATE_READY_TO_APPLY),
+            self._create_fake_strategy(vim.STATE_APPLYING),
+            self._create_fake_strategy(vim.STATE_APPLY_FAILED),
         ]
 
         # API calls acts as expected
-        self.vim_client.apply_strategy.return_value = STRATEGY_APPLYING
+        self.vim_client.apply_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_APPLYING)
 
         # invoke the strategy state operation on the orch thread
         self.worker.perform_state_action(self.strategy_step)
@@ -131,10 +126,12 @@ class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
         # first api query is before the apply
         # test where it never progresses past 'applying'
         self.vim_client.get_strategy.side_effect = itertools.chain(
-            [STRATEGY_READY_TO_APPLY, ], itertools.repeat(STRATEGY_APPLYING))
+            [self._create_fake_strategy(vim.STATE_READY_TO_APPLY), ],
+            itertools.repeat(self._create_fake_strategy(vim.STATE_APPLYING)))
 
         # API calls acts as expected
-        self.vim_client.apply_strategy.return_value = STRATEGY_APPLYING
+        self.vim_client.apply_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_APPLYING)
 
         # invoke the strategy state operation on the orch thread
         self.worker.perform_state_action(self.strategy_step)
@@ -153,8 +150,8 @@ class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
         # first api query is what already exists in applying state
         # remainder are during the loop
         self.vim_client.get_strategy.side_effect = [
-            STRATEGY_APPLYING,
-            STRATEGY_APPLIED,
+            self._create_fake_strategy(vim.STATE_APPLYING),
+            self._create_fake_strategy(vim.STATE_APPLIED),
         ]
 
         # invoke the strategy state operation on the orch thread
@@ -173,7 +170,7 @@ class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
         # first api query is what already exists
         # remainder are during the loop
         self.vim_client.get_strategy.side_effect = [
-            STRATEGY_APPLY_FAILED,
+            self._create_fake_strategy(vim.STATE_APPLY_FAILED),
         ]
 
         # invoke the strategy state operation on the orch thread
@@ -185,3 +182,92 @@ class TestFwUpdateApplyingVIMStrategyStage(TestFwUpdateState):
         # Failure case
         self.assert_step_updated(self.strategy_step.subcloud_id,
                                  consts.STRATEGY_STATE_FAILED)
+
+    def test_applying_vim_strategy_skips_without_subcloud_strategy(self):
+        """Test applying a VIM strategy skips when there isn't a strategy to apply"""
+
+        self.vim_client.get_strategy.return_value = None
+
+        self.worker.perform_state_action(self.strategy_step)
+
+        self.vim_client.apply_strategy.assert_not_called()
+
+        self.assert_step_updated(
+            self.strategy_step.subcloud_id,
+            consts.STRATEGY_STATE_FINISHING_FW_UPDATE
+        )
+
+    @mock.patch.object(BaseState, 'stopped', return_value=True)
+    def test_applying_vim_strategy_fails_when_strategy_stops(self, _):
+        """Test applying a VIM strategy fails when strategy stops"""
+
+        self.vim_client.get_strategy.side_effect = [
+            self._create_fake_strategy(vim.STATE_READY_TO_APPLY)
+        ]
+
+        self.vim_client.apply_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_APPLYING)
+
+        self.worker.perform_state_action(self.strategy_step)
+
+        self.assert_step_updated(
+            self.strategy_step.subcloud_id, consts.STRATEGY_STATE_FAILED
+        )
+
+    def test_applying_vim_strategy_fails_on_max_failed_queries(self):
+        """Test applying a VIM strategy fails when max_failed_queries is reached
+
+        In this case, the DEFAULT_MAX_WAIT_ATTEMPTS must be greater than
+        DEFAULT_MAX_FAILED_QUERIES in order to throw the correct exception
+        """
+
+        self.vim_client.get_strategy.side_effect = [
+            self._create_fake_strategy(vim.STATE_READY_TO_APPLY),
+            self._create_fake_strategy(vim.STATE_APPLYING),
+            Exception(),
+            Exception(),
+            Exception()
+        ]
+
+        self.vim_client.apply_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_APPLYING)
+
+        self.worker.perform_state_action(self.strategy_step)
+
+        self.assert_step_updated(
+            self.strategy_step.subcloud_id, consts.STRATEGY_STATE_FAILED
+        )
+
+    def test_applying_vim_strategy_fails_when_second_subcloud_strategy_is_none(self):
+        """Test applying a VIM strategy fails without second subcloud strategy"""
+
+        self.vim_client.get_strategy.side_effect = [
+            self._create_fake_strategy(vim.STATE_READY_TO_APPLY),
+            None
+        ]
+
+        self.vim_client.apply_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_APPLYING)
+
+        self.worker.perform_state_action(self.strategy_step)
+
+        self.assert_step_updated(
+            self.strategy_step.subcloud_id, consts.STRATEGY_STATE_FAILED
+        )
+
+    def test_applying_vim_strategy_fails_with_invalid_strategy(self):
+        """Test applying a VIM strategy fails with an invalid strategy"""
+
+        self.vim_client.get_strategy.side_effect = [
+            self._create_fake_strategy(vim.STATE_READY_TO_APPLY),
+            self._create_fake_strategy(vim.STATE_ABORTED),
+        ]
+
+        self.vim_client.apply_strategy.return_value = \
+            self._create_fake_strategy(vim.STATE_APPLYING)
+
+        self.worker.perform_state_action(self.strategy_step)
+
+        self.assert_step_updated(
+            self.strategy_step.subcloud_id, consts.STRATEGY_STATE_FAILED
+        )
