a61eb24d6050804f0ea2c3ce2e19c00ab12f2bc3
Hugo Brito <hugo.brito@windriver.com>
2024-05-16 17:57:18 -0300

    Adapt VIM Strategy states for USM
    This commit adapts the CreatingVIMStrategyState to retrieve
    the release data when creating a sw-deploy strategy.
    
    Test Plan:
    PASS: Perform a software orchestration
    - Check that a sw-deploy-strategy was created successfully.
    
    Story: 2010676
    Task: 50110
    
    Change-Id: I088eb343ff174a500fe1b83bdb8d6c4229485085
    Signed-off-by: Hugo Brito <hugo.brito@windriver.com>

--- distributedcloud/dcmanager/orchestrator/states/software/create_vim_software_strategy.py
+++ distributedcloud/dcmanager/orchestrator/states/software/create_vim_software_strategy.py
@@ -1,22 +1,21 @@
 #
-# Copyright (c) 2023 Wind River Systems, Inc.
+# Copyright (c) 2023-2024 Wind River Systems, Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from dccommon.drivers.openstack import vim
 from dcmanager.common import consts
-from dcmanager.orchestrator.states.base import BaseState
+from dcmanager.orchestrator.states.creating_vim_strategy import \
+    CreatingVIMStrategyState
 
 
-class CreateVIMSoftwareStrategyState(BaseState):
+class CreateVIMSoftwareStrategyState(CreatingVIMStrategyState):
     """Create VIM Software Strategy software orchestration state"""
 
     def __init__(self, region_name):
         super(CreateVIMSoftwareStrategyState, self).__init__(
             next_state=consts.STRATEGY_STATE_SW_APPLY_VIM_STRATEGY,
-            region_name=region_name
+            region_name=region_name,
+            strategy_name=vim.STRATEGY_NAME_SW_USM
         )
-
-    def perform_state_action(self, strategy_step):
-        """Create VIM Software Strategy region status"""
-        return self.next_state
