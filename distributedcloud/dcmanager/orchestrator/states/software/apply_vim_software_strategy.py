f7862843f411310e0e7d47911c1b18b0cddee382
Hugo Brito <hugo.brito@windriver.com>
2024-05-27 14:31:47 -0300

    Enable dc orchestration VIM strategy apply for sw-deploy
    This commit enables the apply_vim_software_strategy state
    for USM type of orchestration. The sw-deploy strategy will
    then be applied and VIM on the subcloud will in turn carry
    out a series of software deploy actions on the subcloud.
    
    Test Plan (in-service and reboot required patches):
    PASS: Execute the sw-deploy-strategy create/apply for subcloud.
    - Upload/Apply/Install the release on the system controller.
    - Upload the release on the subcloud.
    - Manage the subcloud.
      - The subcloud should be out-of-sync for USM.
    - Create the sw-deploy-strategy <release-id>.
    - Apply the sw-deploy-strategy.
    - The subcloud should have the release in the deployed state.
      - The subcloud should be in-sync for USM.
    
    Story: 2010676
    Task: 50199
    
    Change-Id: Id6bf06e38323b42f4a5e6e81b9c6237d315bf918
    Signed-off-by: Hugo Brito <hugo.brito@windriver.com>

--- distributedcloud/dcmanager/orchestrator/states/software/apply_vim_software_strategy.py
+++ distributedcloud/dcmanager/orchestrator/states/software/apply_vim_software_strategy.py
@@ -4,19 +4,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from dccommon.drivers.openstack import vim
 from dcmanager.common import consts
-from dcmanager.orchestrator.states.base import BaseState
+from dcmanager.orchestrator.states.applying_vim_strategy import \
+    ApplyingVIMStrategyState
 
 
-class ApplyVIMSoftwareStrategyState(BaseState):
+class ApplyVIMSoftwareStrategyState(ApplyingVIMStrategyState):
     """Apply VIM Software Strategy software orchestration state"""
 
     def __init__(self, region_name):
         super().__init__(
             next_state=consts.STRATEGY_STATE_SW_FINISH_STRATEGY,
-            region_name=region_name
+            region_name=region_name,
+            strategy_name=vim.STRATEGY_NAME_SW_USM
         )
-
-    def perform_state_action(self, strategy_step):
-        """Apply VIM Software Strategy region status"""
-        return self.next_state
--- distributedcloud/dcmanager/tests/unit/orchestrator/states/software/test_apply_vim_software_strategy.py
+++ distributedcloud/dcmanager/tests/unit/orchestrator/states/software/test_apply_vim_software_strategy.py
@@ -4,9 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import mock
+
+from dccommon.drivers.openstack import vim
 from dcmanager.common import consts
+from dcmanager.tests.unit.common import fake_strategy
+from dcmanager.tests.unit.fakes import FakeVimStrategy
 from dcmanager.tests.unit.orchestrator.states.software.test_base import \
     TestSoftwareOrchestrator
+
+STRATEGY_READY_TO_APPLY = FakeVimStrategy(state=vim.STATE_READY_TO_APPLY)
+STRATEGY_APPLYING = FakeVimStrategy(state=vim.STATE_APPLYING)
+STRATEGY_APPLIED = FakeVimStrategy(state=vim.STATE_APPLIED)
+RELEASE_ID = "starlingx-9.0.1"
 
 
 class TestApplyVIMSoftwareStrategyState(TestSoftwareOrchestrator):
@@ -18,14 +28,56 @@ class TestApplyVIMSoftwareStrategyState(TestSoftwareOrchestrator):
         # Add the subcloud being processed by this unit test
         self.subcloud = self.setup_subcloud()
 
+        # Create default strategy with release parameter
+        extra_args = {"release_id": RELEASE_ID}
+        self.strategy = fake_strategy.create_fake_strategy(
+            self.ctx, self.DEFAULT_STRATEGY_TYPE, extra_args=extra_args
+        )
+
         # Add the strategy_step state being processed by this unit test
         self.strategy_step = self.setup_strategy_step(
             self.subcloud.id, consts.STRATEGY_STATE_SW_APPLY_VIM_STRATEGY)
 
+        # Mock the API calls made by the state
+        self.vim_client.get_strategy = mock.MagicMock()
+        self.vim_client.apply_strategy = mock.MagicMock()
+
+    @mock.patch(
+        "dcmanager.orchestrator.states.applying_vim_strategy."
+        "DEFAULT_MAX_FAILED_QUERIES",
+        3,
+    )
+    @mock.patch(
+        "dcmanager.orchestrator.states.applying_vim_strategy."
+        "DEFAULT_MAX_WAIT_ATTEMPTS",
+        3,
+    )
+    @mock.patch(
+        "dcmanager.orchestrator.states.applying_vim_strategy.WAIT_INTERVAL", 1
+    )
+    @mock.patch(
+        "dcmanager.orchestrator.states.applying_vim_strategy."
+        "ApplyingVIMStrategyState.__init__.__defaults__",
+        (3, 1),
+    )
     def test_apply_vim_software_strategy_success(self):
         """Test apply vim software strategy when the API call succeeds."""
 
+        # first api query is before the apply
+        # remaining api query results are after the apply is invoked
+        self.vim_client.get_strategy.side_effect = [
+            STRATEGY_READY_TO_APPLY,
+            STRATEGY_APPLYING,
+            STRATEGY_APPLIED,
+        ]
+
+        # API calls acts as expected
+        self.vim_client.apply_strategy.return_value = STRATEGY_APPLYING
+
         self.worker.perform_state_action(self.strategy_step)
+
+        # Assert apply_strategy is called with the correct strategy name
+        self.vim_client.apply_strategy.assert_called_with(strategy_name="sw-upgrade")
 
         # On success, the state should transition to the next state
         self.assert_step_updated(
